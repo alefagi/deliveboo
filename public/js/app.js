3c8a72fc2dedc1f9bb54210a8d156850971eb137
Riccarpa <riccardopagni99@gmail.com>
2021-11-05 11:31:22 +0100

    git ignore

--- public/js/app.js
+++ public/js/app.js
@@ -50293,8 +50293,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\mobale\Desktop\codice_progetto_finale\deliveboo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\mobale\Desktop\codice_progetto_finale\deliveboo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\ricca\Desktop\Progetto-finale\deliveboo-1\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\ricca\Desktop\Progetto-finale\deliveboo-1\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
