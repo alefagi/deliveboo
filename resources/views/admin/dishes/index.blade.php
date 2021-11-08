85a1007c5a7f6f5ee49c9b661251e95e53784159
alefagi <iltu@libero.it>
2021-11-09 00:10:11 +0100

    completed dishes crud

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -3,23 +3,24 @@
 
 @section('content')
 <header class="my-5 d-flex justify-content-between align-items-center">
-    <h1 class="text-center w-100">I miei piatti</h1>
+    <h1 class="text-center w-100">My Dishes</h1>
 </header>
 <div class="container">
      <div class="row">
          <nav class="col-4">@include('admin.includes.links')</nav>
-     
-     
                 <div class="col-8">
                     <table class="table table-dark">
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
-                                <th scope="col">Titolo</th>
-                                <th scope="col">Descrizione</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Description</th>
                                 <th scope="col">Cover</th>
-                                <th scope="col">Prezzo</th>
-                                <th scope="col"></th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Visibility</th>
+                                <th scope="col">View</th>
+                                <th scope="col">Edit</th>
+                                <th scope="col">Remove</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -28,25 +29,22 @@
                                     <th scope="row">{{ $dish->id }}</th>
                                     <td>{{ $dish->name }}</td>
                                     <td>{{ $dish->description }}</td>
-                                    <td><img src="{{$dish->cover}}" class="img-fluid" alt=""></td>
+                                    <td><img src="{{ $dish->cover }}" class="img-fluid" alt="{{ $dish->name }}"></td>
                                     <td>{{ $dish->price }}</td>
-     
-     
-                                    {{-- <td class="d-flex justify-content-end">
-                                        <a href="{{ route('admin.posts.show', $post->id) }}" class="btn btn-primary">Vai</a>
-                                        <a href="{{ route('admin.posts.edit', $post->id) }}"
-                                            class="btn btn-warning ml-2">Modifica</a>
-                                        <form action="{{ route('admin.posts.destroy', $post->id) }}" method="post"
-                                            class="delete-button">
+                                    <td>{{ $dish->visible }}</td>
+                                    <td><a href="{{ route('admin.dishes.show', $dish->id) }}" class="btn btn-primary">View</a></td>
+                                    <td><a href="{{ route('admin.dishes.edit', $dish->id) }}" class="btn btn-warning">Edit</a></td>
+                                    <td>
+                                        <form action="{{ route('admin.dishes.destroy', $dish->id)}}" method="post" class="ml-2 delete-button">
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" class="btn btn-danger ml-2">Elimina</button>
-                                        </form>
-                                    </td> --}}
+                                            <button type="submit" class="btn btn-danger">Delete</button>
+                                        </form>    
+                                    </td>
                                 </tr>
                             @empty
                                 <tr>
-                                    <td colspan="3" class="text-center">Non ci sono piatti da visualizzare.</td>
+                                    <td colspan="9" class="text-center">There are no dishes to display.</td>
                                 </tr>
                             @endforelse
                         </tbody>
--- resources/views/admin/includes/links.blade.php
+++ resources/views/admin/includes/links.blade.php
@@ -1,9 +1,4 @@
-
-
-<a class="d-block" href="{{route('admin.dishes.index')}}">Tutti i tuoi piatti</a>
-<a class="d-block" href="{{route('admin.dishes.create')}}">Aggiungi un nuovo piatto</a>
-<a class="d-block" href="{{route('admin.orders.index')}}">I tuoi ordini</a>
-<a class="d-block" href="# ">Torna alla home</a>
-
-
-
+<a class="d-block" href="{{route('admin.dishes.index')}}">All your dishes</a>
+<a class="d-block" href="{{route('admin.dishes.create')}}">Add a new dish</a>
+<a class="d-block" href="{{route('admin.orders.index')}}">Your orders</a>
+<a class="d-block" href="#">Back to home</a>
