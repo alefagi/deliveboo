1d4f29c8f28cbc67202a3df29fe16d601a49e072
Riccarpa <riccardopagni99@gmail.com>
2021-11-09 11:21:28 +0100

    ancora grafica

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,5 +2,13 @@
 
 
 @section('content')
- @include('admin.includes.links')
+<header class="my-5 d-flex justify-content-between align-items-center">
+    <h1 class="text-center w-100">My statistics</h1>
+</header>
+ <div class="container">
+     <div class="row">
+         <div class="col-4">@include('admin.includes.links')</div>
+         <div class="col-8">Statistiche</div>
+     </div>
+ </div>
 @endsection
--- resources/views/admin/includes/links.blade.php
+++ resources/views/admin/includes/links.blade.php
@@ -1,3 +1,4 @@
+<a class="d-block" href="{{route('admin.home')}}">Statistics</a>
 <a class="d-block" href="{{route('admin.dishes.index')}}">All your dishes</a>
 <a class="d-block" href="{{route('admin.dishes.create')}}">Add a new dish</a>
 <a class="d-block" href="{{route('admin.orders.index')}}">Your orders</a>
