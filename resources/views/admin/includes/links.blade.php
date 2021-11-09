8359b4c7412ddef877593080b1eecb1965a894e5
Riccarpa <riccardopagni99@gmail.com>
2021-11-09 11:31:09 +0100

    back to home

--- resources/views/admin/includes/links.blade.php
+++ resources/views/admin/includes/links.blade.php
@@ -2,4 +2,4 @@
 <a class="d-block" href="{{route('admin.dishes.index')}}">All your dishes</a>
 <a class="d-block" href="{{route('admin.dishes.create')}}">Add a new dish</a>
 <a class="d-block" href="{{route('admin.orders.index')}}">Your orders</a>
-<a class="d-block" href="#">Back to home</a>
+<a class="d-block" href="{{url('/')}}">Back to home</a>
