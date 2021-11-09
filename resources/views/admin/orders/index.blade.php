71250e9c9e15697b7717da285570e31e7108b72a
Riccarpa <riccardopagni99@gmail.com>
2021-11-09 11:24:38 +0100

    order set up

---
+++ resources/views/admin/orders/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+
+@section('content')
+<header class="my-5 d-flex justify-content-between align-items-center">
+    <h1 class="text-center w-100">My orders</h1>
+</header>
+<div class="container">
+     <div class="row">
+         <nav class="col-4">@include('admin.includes.links')</nav>
+        <div class="col-8">
+
+
+            
+        </div>
+ </div>
+@endsection
