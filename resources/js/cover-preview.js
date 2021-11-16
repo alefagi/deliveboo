dd29f99bfbf2c3f2afed85e1334d11ecd3a0ba82
alefagi <iltu@libero.it>
2021-11-16 10:31:54 +0100

    fixed variables name

--- resources/js/cover-preview.js
+++ resources/js/cover-preview.js
@@ -4,30 +4,32 @@ const coverElementUrl = document.getElementById('cover-url');
 const coverPreviewElement = document.getElementById('cover-preview');
 
 coverElementFile.addEventListener('change', function(){
-  const coverInput = document.getElementById("cover-file").files;
-  console.log(coverInput);
+  const coverInput = coverElementFile.files;
   
-  if (coverInput) {
+  if(coverInput) {
     const fileReader = new FileReader();
     fileReader.onload = function (event) {
-      document.getElementById('cover-preview').setAttribute('src', event.target.result);
+      coverPreviewElement.setAttribute('src', event.target.result);
     };
 
     fileReader.readAsDataURL(coverInput[0]);
   }
+  console.log(coverInput);
+
 });
 
 coverElementUrl.addEventListener('change', function(){
-  const coverInput = document.getElementById("cover-url").value;
-  console.log(coverInput);
-
+  const coverInput = coverElementUrl.value;
+  
   if(coverInput) {
     const url = this.value;
     if(url) {
       coverPreviewElement.setAttribute('src', url);
+      console.log(url);
     }
     else {
       coverPreviewElement.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg');
+      console.log('prova');
     }
   }
 });
