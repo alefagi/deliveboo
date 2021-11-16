a18247975d3fe0c9bac3d53e61e474def2547136
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-11-17 00:06:49 +0800

    add loading

---
+++ resources/js/components/Load.vue
@@ -0,0 +1,30 @@
+<template>
+  <section id="loader">
+    <h1>Loading...</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Load",
+};
+</script>
+
+<style>
+#loader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  color: rgb(250, 7, 7);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url();
+}
+img {
+  height: 500px;
+  width: 500px;
+}
+</style>
--- resources/js/components/RestaurantList.vue
+++ resources/js/components/RestaurantList.vue
@@ -1,87 +1,119 @@
 <template>
-    <div class="restaurants-list">
-        <div>
-            <h3>Ricerca avanzata</h3>
-            <div><input class="form-string" type="text" id="serached-string" v-model="searchedString" placeholder="Cerca per nome..."></div>
-            <div class="form-cuisine" v-for="cuisine in cuisines" :key="cuisine.index">
-                <input type="checkbox" :id="cuisine.name" :value="cuisine.id" v-model="checkedCuisines">
-                <label class="mr-2" :for="cuisine.name">{{cuisine.name}}</label>
-            </div>
-            <div class="restaurant" v-for="restaurant in searchedRestaurants" :key="restaurant.id">
-                <RestaurantCard :restaurant="restaurant" />
-            </div>
-        </div>
+  <Load v-if="isLoading" />
+  <div class="restaurants-list" v-else>
+    <div>
+      <h3>Ricerca avanzata</h3>
+      <div>
+        <input
+          class="form-string"
+          type="text"
+          id="serached-string"
+          v-model="searchedString"
+          placeholder="Cerca per nome..."
+        />
+      </div>
+      <div
+        class="form-cuisine"
+        v-for="cuisine in cuisines"
+        :key="cuisine.index"
+      >
+        <input
+          type="checkbox"
+          :id="cuisine.name"
+          :value="cuisine.id"
+          v-model="checkedCuisines"
+        />
+        <label class="mr-2" :for="cuisine.name">{{ cuisine.name }}</label>
+      </div>
+      <div
+        class="restaurant"
+        v-for="restaurant in searchedRestaurants"
+        :key="restaurant.id"
+      >
+        <RestaurantCard :restaurant="restaurant" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import RestaurantCard from "./RestaurantCard.vue";
+import Load from "./Load.vue";
+
 export default {
-    name: 'RestaurantList',
-    components: {
-        RestaurantCard,
-    },
-    data() {
+  name: "RestaurantList",
+  components: {
+    RestaurantCard,
+    Load,
+  },
+  data() {
     return {
-        restaurants: [],
-        cuisines: [],
-        cuisinesIds: [],
-        checkedCuisines: [],
-        searchedString: "",
-    }
-    },
-    created: function(){
-        axios.get('http://127.0.0.1:8000/api/users').then(res => {
-            this.restaurants = res.data;
-        });
-        axios.get('http://127.0.0.1:8000/api/cuisines').then(res => {
-            this.cuisines = res.data;
-            this.cuisinesIds = this.cuisines.map((cuisine) => {return cuisine.id});
-        });
-    },
-    computed: {
-        searchedRestaurants() {
-            if(this.checkedCuisines.length == 0 && this.searchedString == "") {
-                console.log("hey")
-                return this.restaurants;
-            }
+      isLoading: true,
+      restaurants: [],
+      cuisines: [],
+      cuisinesIds: [],
+      checkedCuisines: [],
+      searchedString: "",
+    };
+  },
+  created: function () {
+    axios.get("http://127.0.0.1:8000/api/users").then((res) => {
+      this.restaurants = res.data;
+      this.isLoading = false;
+    });
+    axios.get("http://127.0.0.1:8000/api/cuisines").then((res) => {
+      this.cuisines = res.data;
+      this.isLoading = false;
+      this.cuisinesIds = this.cuisines.map((cuisine) => {
+        return cuisine.id;
+      });
+    });
+  },
+  computed: {
+    searchedRestaurants() {
+      if (this.checkedCuisines.length == 0 && this.searchedString == "") {
+        console.log("hey");
+        return this.restaurants;
+      }
 
-            return this.stringRestaurants.filter((restaurant) => {
-                let auxBoolean = true;
-                this.checkedCuisines.forEach(cuisine => {
-                    if(!restaurant.cuisines.map(i => i['id']).includes(cuisine)){
-                        auxBoolean = false;
-                    }
-                })
-                return auxBoolean
-            });
-        },
-        stringRestaurants() {
-            if(this.searchedString == "") {
-                return this.restaurants;
-            }
-
-            return this.restaurants.filter((restaurant) => {
-                return restaurant.name.toLowerCase().includes(this.searchedString.toLowerCase()) 
-            });
-        }
+      return this.stringRestaurants.filter((restaurant) => {
+        let auxBoolean = true;
+        this.checkedCuisines.forEach((cuisine) => {
+          if (!restaurant.cuisines.map((i) => i["id"]).includes(cuisine)) {
+            auxBoolean = false;
+          }
+        });
+        return auxBoolean;
+      });
     },
-}
+    stringRestaurants() {
+      if (this.searchedString == "") {
+        return this.restaurants;
+      }
+
+      return this.restaurants.filter((restaurant) => {
+        return restaurant.name
+          .toLowerCase()
+          .includes(this.searchedString.toLowerCase());
+      });
+    },
+  },
+};
 </script>
 
 <style>
-.restaurants-list{
-    width: 70%;
-    margin: 0 auto;
+.restaurants-list {
+  width: 70%;
+  margin: 0 auto;
 }
-.form-cuisine{
-    display: inline-block;
-    margin-bottom: 20px;
+.form-cuisine {
+  display: inline-block;
+  margin-bottom: 20px;
 }
-.form-string{
-    margin-bottom: 20px;
+.form-string {
+  margin-bottom: 20px;
 }
 .restaurant {
-    display: inline-block;
+  display: inline-block;
 }
 </style>
