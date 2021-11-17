abcaab61f87c623c867acc5a3ce1b7c7938fa643
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-11-17 22:35:54 +0800

    Added homepage corrections

--- resources/js/components/Jumbotron.vue
+++ resources/js/components/Jumbotron.vue
@@ -1,51 +1,49 @@
 <template>
-    <div class="jumbotron">
-        <div class="jumbotron-text">
-            <h1>DeliveBoo</h1>
-            <p>We deliver happiness at your doorstep.</p>
-            <p class="p-special">The food you love one click away.</p>
-        </div>
+  <div class="jumbotron">
+    <div class="jumbotron-text">
+      <h1>DeliveBoo</h1>
+      <p>We deliver happiness at your doorstep.</p>
+      <p class="p-special">The food you love one click away.</p>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
 .jumbotron {
-    position: relative;
-    z-index: -1;
-    top: -60px;
+  position: relative;
+  z-index: -1;
+  top: -60px;
 
-    background-image: url(https://images.pexels.com/photos/349609/pexels-photo-349609.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260);
-    background-size: cover;
-    background-position: top;
-    height: 450px;
-    width: 100%;
+  background-image: url(https://images.pexels.com/photos/349609/pexels-photo-349609.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260);
+  background-size: cover;
+  background-position: top;
+  height: 550px;
+  width: 100%;
 
-    position: relative;
+  position: relative;
 
-    font-size: 30px;
-    line-height: 10px;
+  font-size: 30px;
+  line-height: 10px;
 }
 .jumbotron-text {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 
-    text-align: center;
+  text-align: center;
 }
 h1 {
-    font-family: 'Cedarville Cursive', cursive;
-    font-style: italic;
-    font-size: 120px;
+  font-family: "Cedarville Cursive", cursive;
+  font-style: italic;
+  font-size: 120px;
 }
 .p-special {
-    font-style: italic;
-    font-size: 20px;
+  font-style: italic;
+  font-size: 20px;
 }
 </style>
--- resources/js/components/RestaurantList.vue
+++ resources/js/components/RestaurantList.vue
@@ -1,22 +1,77 @@
 <template>
-    <Load v-if="isLoading" />
-    <div class="restaurants-list" v-else>
-        <Jumbotron />
-        <div class="container">
-            <h3>Ricerca avanzata</h3>
-            <div><input class="form-string" type="text" id="serached-string" v-model="searchedString" placeholder="Cerca per nome..."></div>
-            <div class="form-cuisine" v-for="cuisine in cuisines" :key="cuisine.index">
-                <input class="cuisine-checkbox" type="checkbox" :id="cuisine.name" :value="cuisine.id" v-model="checkedCuisines">
-                <label class="cuisine-img mr-2" 
-                :class="checkedCuisines.includes(cuisine.id) ? 'cuisine-img-checked' : ''" :for="cuisine.name"><img :src="cuisine.cover" alt=""></label>
-            </div>
-            <div class="restaurant d-flex flex-wrap mb-5">
-                <div @click="prev()" class="carusel-btn">prev</div>
-                <RestaurantCard v-for="restaurant in presentRestaurants" :key="restaurant.id" :restaurant="restaurant" />
-                <div @click="next()" class="carusel-btn">next</div>
-            </div>
-        </div>
+  <Load v-if="isLoading" />
+  <div class="restaurants-list" v-else>
+    <Jumbotron />
+    <div class="search-bar">
+      <h3>Ricerca avanzata</h3>
+      <div>
+        <input
+          class="form-string"
+          type="text"
+          id="serached-string"
+          v-model="searchedString"
+          placeholder="Cerca per nome..."
+        />
+      </div>
     </div>
+    <div class="container text-center">
+      <div
+        class="form-cuisine"
+        v-for="cuisine in cuisines"
+        :key="cuisine.index"
+      >
+        <input
+          class="cuisine-checkbox"
+          type="checkbox"
+          :id="cuisine.name"
+          :value="cuisine.id"
+          v-model="checkedCuisines"
+        />
+        <label
+          class="cuisine-img mr-2"
+          :class="
+            checkedCuisines.includes(cuisine.id) ? 'cuisine-img-checked' : ''
+          "
+          :for="cuisine.name"
+          ><img :src="cuisine.cover" alt=""
+        /></label>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="restaurant row mb-5">
+        <div
+          @click="prev()"
+          class="
+            carusel-btn
+            col
+            d-flex
+            align-items-center
+            justify-content-center
+          "
+        >
+          <i class="fas fa-angle-left fa-4x clickable"></i>
+        </div>
+        <RestaurantCard
+          v-for="restaurant in presentRestaurants"
+          :key="restaurant.id"
+          :restaurant="restaurant"
+          class="col"
+        />
+        <div
+          @click="next()"
+          class="
+            carusel-bt
+            col
+            d-flex
+            align-items-center
+            justify-content-center
+          "
+        >
+          <i class="fas fa-angle-right fa-4x clickable"></i>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 
@@ -26,122 +81,141 @@ import Load from "./Load.vue";
 import Jumbotron from "./Jumbotron.vue";
 
 export default {
-    name: 'RestaurantList',
-    components: {
-        RestaurantCard,
-        Load,
-        Jumbotron,
-    },
-    data() {
+  name: "RestaurantList",
+  components: {
+    RestaurantCard,
+    Load,
+    Jumbotron,
+  },
+  data() {
     return {
-        isLoading: true,
-        restaurants: [],
-        cuisines: [],
-        cuisinesIds: [],
-        checkedCuisines: [],
-        searchedString: "",
+      isLoading: true,
+      restaurants: [],
+      cuisines: [],
+      cuisinesIds: [],
+      checkedCuisines: [],
+      searchedString: "",
 
-        position: 0,
-    }
-    },
-    created: function(){
-        axios.get('http://127.0.0.1:8000/api/users').then(res => {
-            this.restaurants = res.data;
-            this.isLoading = false;
+      position: 0,
+    };
+  },
+  created: function () {
+    axios.get("http://127.0.0.1:8000/api/users").then((res) => {
+      this.restaurants = res.data;
+      this.isLoading = false;
+    });
+    axios.get("http://127.0.0.1:8000/api/cuisines").then((res) => {
+      this.cuisines = res.data;
+      this.cuisinesIds = this.cuisines.map((cuisine) => {
+        return cuisine.id;
+      });
+      this.isLoading = false;
+    });
+  },
+  computed: {
+    searchedRestaurants() {
+      this.position = 0;
+      if (this.checkedCuisines.length == 0 && this.searchedString == "") {
+        return this.restaurants;
+      }
+
+      return this.stringRestaurants.filter((restaurant) => {
+        let auxBoolean = true;
+        this.checkedCuisines.forEach((cuisine) => {
+          if (!restaurant.cuisines.map((i) => i["id"]).includes(cuisine)) {
+            auxBoolean = false;
+          }
         });
-        axios.get('http://127.0.0.1:8000/api/cuisines').then(res => {
-            this.cuisines = res.data;
-            this.cuisinesIds = this.cuisines.map((cuisine) => {return cuisine.id});
-            this.isLoading = false;
-        });
+        return auxBoolean;
+      });
     },
-    computed: {
-        searchedRestaurants() {
-            this.position=0;
-            if(this.checkedCuisines.length == 0 && this.searchedString == "") {
-                return this.restaurants;
-            }
+    stringRestaurants() {
+      if (this.searchedString == "") {
+        return this.restaurants;
+      }
 
-            return this.stringRestaurants.filter((restaurant) => {
-                let auxBoolean = true;
-                this.checkedCuisines.forEach(cuisine => {
-                    if(!restaurant.cuisines.map(i => i['id']).includes(cuisine)){
-                        auxBoolean = false;
-                    }
-                })
-                return auxBoolean
-            });
-        },
-        stringRestaurants() {
-            if(this.searchedString == "") {
-                return this.restaurants;
-            }
+      return this.restaurants.filter((restaurant) => {
+        return restaurant.name
+          .toLowerCase()
+          .includes(this.searchedString.toLowerCase());
+      });
+    },
+    presentRestaurants() {
+      let auxRestaurants = [];
+      for (
+        let i = this.position;
+        i < this.position + 3 && i < this.searchedRestaurants.length;
+        i++
+      ) {
+        auxRestaurants.push(this.searchedRestaurants[i]);
+      }
 
-            return this.restaurants.filter((restaurant) => {
-                return restaurant.name.toLowerCase().includes(this.searchedString.toLowerCase()) 
-            });
-        },
-        presentRestaurants() {
-            let auxRestaurants = [];
-            for(let i = this.position; (i < this.position+3 && i < this.searchedRestaurants.length) ; i++) {
-                auxRestaurants.push(this.searchedRestaurants[i]);
-
-            }
-
-            if(auxRestaurants.length<3 && this.searchedRestaurants.length!=auxRestaurants.length) {
-                for(let i = 0; (i < 4 - auxRestaurants.length && i < this.searchedRestaurants.length - auxRestaurants.length) ; i++ ) {
-                    auxRestaurants.push(this.searchedRestaurants[i]);
-                }
-            }
-            return auxRestaurants;
+      if (
+        auxRestaurants.length < 3 &&
+        this.searchedRestaurants.length != auxRestaurants.length
+      ) {
+        for (
+          let i = 0;
+          i < 4 - auxRestaurants.length &&
+          i < this.searchedRestaurants.length - auxRestaurants.length;
+          i++
+        ) {
+          auxRestaurants.push(this.searchedRestaurants[i]);
         }
+      }
+      return auxRestaurants;
     },
-    methods: {
-        next(){
-           if(this.position < this.searchedRestaurants.length-1) {
-               this.position++
-           } else {
-               this.position = 0;
-           }
-        },
-        prev(){
-            if(this.position > 0) {
-                this.position--
-            } else {
-                this.position = this.searchedRestaurants.length-1;
-            }
-        }
+  },
+  methods: {
+    next() {
+      if (this.position < this.searchedRestaurants.length - 1) {
+        this.position++;
+      } else {
+        this.position = 0;
+      }
     },
-    
-}
+    prev() {
+      if (this.position > 0) {
+        this.position--;
+      } else {
+        this.position = this.searchedRestaurants.length - 1;
+      }
+    },
+  },
+};
 </script>
 
 <style>
-.form-cuisine{
-    display: inline-block;
-    margin-bottom: 20px;
+.search-bar {
+  text-align: center;
+  margin-top: -200px;
+  margin-bottom: 100px;
+}
+.form-cuisine {
+  display: inline-block;
+  margin-bottom: 20px;
 }
 .cuisine-checkbox {
-    display: none;
+  display: none;
 }
 .cuisine-img {
-    text-align: center;
-    padding: 10px;
-    background-color: lightgray;
-    border-radius: 50%;
+  text-align: center;
+  padding: 10px;
+  background-color: lightgray;
+  border-radius: 50%;
 }
 .cuisine-img-checked {
-    background-color: lightblue;
-    border: 1px solid blue;
+  background-color: lightblue;
+  border: 1px solid blue;
 }
 .cuisine-img img {
-    height: 55px;
-    width: 55px;
+  height: 55px;
+  width: 55px;
 }
-.form-string{
-    margin-bottom: 20px;
+.form-string {
+  margin-bottom: 20px;
 }
 .carusel-btn {
-    width: 5%;
+  width: 5%;
 }
 </style>
