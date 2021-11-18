9a5f22e07034b8576ba73e0d70d56bdd6734ff9a
Riccarpa <riccardopagni99@gmail.com>
2021-11-18 17:35:44 +0100

    dish edit+show style

--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -1,80 +1,80 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-      <header class="d-flex justify-content-between align-items-center">
-        <h1>Edit Dish</h1>
-      </header>
+    <div class="row h-100 w-100 m-0">
+      <div id="links" class="col-3">@include('admin.includes.links')</div>
+      <div id="dish-edit" class="col-9">
+        <header class="d-flex justify-content-between align-items-center mb-4">
+          <h1>Edit Dish</h1>
+        </header>
       
-      <form method="post" enctype="multipart/form-data" action="{{ route('admin.dishes.update', $dish->id) }}">
-        @method('PATCH')
-        @csrf
-
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ $dish->name }}">
-            @error('name') 
-              <div class="invalid-feedback">
-                {{ $message }}
-              </div>
-            @enderror
-        </div>
-      
-        <div class="form-group">
-          <label for="description">Description</label>
-          <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="5">{{ $dish->description }}</textarea>
-          @error('description') 
-            <div class="invalid-feedback">
-              {{ $message }}
-            </div>
-          @enderror
-        </div>
-    
-        <div class="form-group mb-2">
-          <label for="cover">Choose a Cover to Upload</label>
-            <input type="file" class="form-control-file @error('cover') is-invalid @enderror" id="cover-file" name="cover" accept="image/*">
-            <input type="text" class="form-control mb-2 @error('cover') is-invalid @enderror" id="cover-url" name="cover" value="{{ old('cover'), $dish->cover }}">
-          @error('cover') 
-            <div class="invalid-feedback">
-              {{ $message }}
-            </div>
-          @enderror
-          <div class="mb-2">
-            <img src="{{ str_starts_with($dish->cover, 'http') ? $dish->cover : asset('storage/' . $dish->cover) }}" alt="cover-preview" class="img-fluid w-50" id="cover-preview">
+        <form method="post" enctype="multipart/form-data" action="{{ route('admin.dishes.update', $dish->id) }}">
+          @method('PATCH')
+          @csrf
+          <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ $dish->name }}">
+              @error('name')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div>
+              @enderror
           </div>
-        </div>
-
-        <div class="form-group">
-            <label for="price">Price</label>
-            <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ $dish->price }}">
-            @error('price') 
+      
+          <div class="form-group">
+            <label for="description">Description</label>
+            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="5">{{ $dish->description }}</textarea>
+            @error('description')
               <div class="invalid-feedback">
                 {{ $message }}
               </div>
             @enderror
-        </div>
-
-        <div class="mb-4">
-          <h6>Tags</h6>
-          @foreach ($tags as $tag)
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" id="tag-{{ $tag->id }}" value="{{ $tag->id }}" name="tags[]" @if (in_array($tag->id, old('tags', $tagsId ?? [] ))) checked @endif>
-              <label class="form-check-label" for="tag-{{ $tag->id }}">{!! $tag->icon !!} {{ $tag->name }} </label>
-            </div>
-          @endforeach
-        </div>
+          </div>
       
-        <div class="form-group">
-          <label for="visible">Visibility</label>
-          <select class="form-control" id="visible" name="visible">
-            <option value="0">Invisible</option>
-            <option value="1" selected>Visible</option>
-          </select>
-        </div>
-    
-        <button type="submit" class="btn btn-success">Save</button>
-        <a href="{{ route('admin.dishes.index') }}" class="btn btn-secondary">Back to dishes list</a>
-    </form>
+          <div class="form-group mb-4">
+            <label for="cover">Choose a Cover to Upload</label>
+              <input type="file" class=" mb-2 form-control-file @error('cover') is-invalid @enderror" id="cover-file" name="cover" accept="image/*">
+              <input type="text" placeholder="URL" class="form-control mb-2 @error('cover') is-invalid @enderror" id="cover-url" name="cover" value="{{ old('cover'), $dish->cover }}">
+            @error('cover')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+            @enderror
+            <div class="mb-2">
+              <img src="{{ str_starts_with($dish->cover, 'http') ? $dish->cover : asset('storage/' . $dish->cover) }}" alt="cover-preview"  id="cover-preview">
+            </div>
+          </div>
+          <div class="form-group">
+              <label for="price">Price</label>
+              <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ $dish->price }}">
+              @error('price')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div>
+              @enderror
+          </div>
+          <div class="mb-4">
+            <h6>Tags</h6>
+            @foreach ($tags as $tag)
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" id="tag-{{ $tag->id }}" value="{{ $tag->id }}" name="tags[]" @if (in_array($tag->id, old('tags', $tagsId ?? [] ))) checked @endif>
+                <label class="form-check-label" for="tag-{{ $tag->id }}">{!! $tag->icon !!} {{ $tag->name }} </label>
+              </div>
+            @endforeach
+          </div>
+      
+          <div class="form-group mb-5">
+            <label for="visible">Visibility</label>
+            <select class="form-control" id="visible" name="visible">
+              <option value="0">Invisible</option>
+              <option value="1" selected>Visible</option>
+            </select>
+          </div>
+      
+          <button type="submit" class="btn btn-success">Save</button>
+          <a href="{{ route('admin.dishes.index') }}" class="btn btn-secondary">Back to dishes list</a>
+      </form>
+      </div>
     </div>
 
     @section('script')
--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -1,28 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-    <h1>{{ $dish->name }}</h1>
-    <img src="{{ str_starts_with($dish->cover, 'http') ? $dish->cover : asset('storage/' . $dish->cover) }}" class="img-fluid" alt="{{ $dish->name }}">
-    <p>{{ $dish->description }}</p>
-    @forelse ($dish->tags as $tag)
-      <div>{!! $tag->icon !!} {{ $tag->name }}</div>
-    @empty -
-    @endforelse
-    <address>Price: {{ $dish->price }}</address>
-    <address>Visibility: {{ $dish->visible }}</address>
-    <div class="d-flex justify-content-end">
-      <a href="{{ route('admin.dishes.edit', $dish->id) }}" class="btn btn-warning ml-2">Edit</a>
+<div class="row h-100 w-100 m-0">
+  <div id="links" class="col-3">@include('admin.includes.links')</div>
 
-      <form action="{{ route('admin.dishes.destroy', $dish->id)}}" method="post" class="ml-2 delete-button">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-danger">Delete</button>
-      </form>
-
-      <a href="{{ route('admin.dishes.index') }}" class="ml-2 btn btn-secondary">Back to dishes list</a>
+    <div id="dish-show" class="col-9">
+      <h1>{{ $dish->name }}</h1>
+      <img src="{{ str_starts_with($dish->cover, 'http') ? $dish->cover : asset('storage/' . $dish->cover) }}" class="img-fluid" alt="{{ $dish->name }}">
+      <p>{{ $dish->description }}</p>
+      @forelse ($dish->tags as $tag)
+        <div>{!! $tag->icon !!} {{ $tag->name }}</div>
+      @empty -
+      @endforelse
+      <address>Price: {{ $dish->price }}</address>
+      <address>Visibility: {{ $dish->visible }}</address>
+      <div class="d-flex justify-content-end">
+        <a href="{{ route('admin.dishes.edit', $dish->id) }}" class="btn btn-warning ml-2">Edit</a>
+        <form action="{{ route('admin.dishes.destroy', $dish->id)}}" method="post" class="ml-2 delete-button">
+          @csrf
+          @method('DELETE')
+          <button type="submit" class="btn btn-danger">Delete</button>
+        </form>
+        <a href="{{ route('admin.dishes.index') }}" class="ml-2 btn btn-secondary">Back to dishes list</a>
+      </div>
+        </div>
     </div>
-  </div>
 
   @section('script')
         <script src="{{ asset('js/confirm-delete.js') }}"></script>
