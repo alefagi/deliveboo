0c5260370692a624db56d52c38640a0f4f474911
Riccarpa <riccardopagni99@gmail.com>
2021-11-18 15:51:12 +0100

    modifichine su create dish

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -17,7 +17,7 @@
                 </div>
               @enderror
           </div>
-          <div class="form-group">
+          <div class="form-group mb-5">
             <label for="description">Description</label>
             <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="5">{{ old('description'), $dish->description }}</textarea>
             @error('description')
@@ -26,20 +26,20 @@
               </div>
             @enderror
           </div>
-          <div class="form-group mb-2">
-            <label for="cover">Choose a Cover to Upload</label>
-            <input type="file" class="form-control-file @error('cover') is-invalid @enderror" id="cover-file" name="cover" accept="image/*">
-            <input type="text" class="form-control mb-2 @error('cover') is-invalid @enderror" id="cover-url" name="cover" value="{{ old('cover'), $dish->cover }}">
+          <div class="form-group mb-4">
+            <label class="mb-2" for="cover">Choose an image from your files or paste an URL</label>
+            <input type="file" class="form-control-file mb-2 @error('cover') is-invalid @enderror" id="cover-file" name="cover" accept="image/*">
+            <input placeholder="Paste an URL" type="text" class="form-control mb-2 @error('cover') is-invalid @enderror" id="cover-url" name="cover" value="{{ old('cover'), $dish->cover }}">
             @error('cover') 
               <div class="invalid-feedback">
                 {{ $message }}
               </div>
             @enderror
             <div class="mb-2">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg" alt="cover-preview" class="img-fluid w-50" id="cover-preview">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg" alt="cover-preview"    id="cover-preview">
             </div>
           </div>
-          <div class="form-group">
+          <div class="form-group mb-4">
               <label for="price">Price</label>
               <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price'), $dish->price }}">
               @error('price')
@@ -58,7 +58,7 @@
               </div>
             @endforeach
           </div>
-          <div class="form-group">
+          <div class="form-group mb-5">
             <label for="visible">Visibility</label>
             <select class="form-control" id="visible" name="visible">
               <option value="0">Invisible</option>
@@ -66,9 +66,11 @@
             </select>
           </div>
         
-          <button type="submit" class="btn btn-success">Save</button>
-        
-          <a href="{{ route('admin.dishes.index') }}" class="btn btn-secondary">Back to dishes list</a>
+          <div class="buttons">
+            <button type="submit" class="btn btn-success">Save</button>
+            <a href="{{ route('admin.dishes.index') }}" class="btn btn-secondary">Back to dishes list</a>
+          </div>
+
         </form>
       </div>
     </div>
