a235b7adafc8539781d35c9218a3c1b96083debd
Riccarpa <riccardopagni99@gmail.com>
2021-11-19 14:44:29 +0100

    .

--- resources/views/admin/includes/links.blade.php
+++ resources/views/admin/includes/links.blade.php
@@ -36,7 +36,7 @@
 </div>
 <hr>
 <a class="link {{ request()->routeIs('admin.home') ? 'active' : '' }}" href="{{route('admin.home')}}"> <i class="far fa-chart-bar fa-2x"></i>Statistics</a>
-<a class="link {{ request()->routeIs('admin.dishes.index') ? 'active' : '' }}" href="{{route('admin.dishes.index')}}"><i class="fas fa-utensils fa-2x"></i>All your dishes</a>
+<a class="link {{ request()->routeIs('admin.dishes.show') ? 'active' : '' }}{{ request()->routeIs('admin.dishes.index') ? 'active' : '' }}" href="{{route('admin.dishes.index')}}"><i class="fas fa-utensils fa-2x"></i>All your dishes</a>
 <a  class="link {{ request()->routeIs('admin.dishes.create') ? 'active' : '' }}" href="{{route('admin.dishes.create')}}"><i class="fas fa-plus fa-2x"></i> Add a new dish</a>
-<a  class="link {{ request()->routeIs('admin.orders.index') ? 'active' : '' }}" href="{{route('admin.orders.index')}}"><i class="fas fa-shopping-cart fa-2x"></i>Your orders</a>
+<a  class="link {{ request()->routeIs('admin.orders.show') ? 'active' : '' }} {{ request()->routeIs('admin.orders.index') ? 'active' : '' }}" href="{{route('admin.orders.index')}}"><i class="fas fa-shopping-cart fa-2x"></i>Your orders</a>
 <a  class="link" href="{{url('/')}}"><i class="fas fa-home fa-2x"></i>Back to home</a>
