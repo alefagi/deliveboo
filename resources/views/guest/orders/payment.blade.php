207fe681215998708850b6a3b51dcb9e34d88772
sergiojanko <janko.sergio1998@gmail.com>
2021-11-19 16:34:05 +0100

    fixed layout

--- resources/views/guest/orders/payment.blade.php
+++ resources/views/guest/orders/payment.blade.php
@@ -1,42 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    @if (session('success_message'))
-        <div class="alert alert-success">
-            {{ session('success_message') }}
-        </div>
-    @endif
+@extends('layouts.guest')
 
-    @if(count($errors) > 0)
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+@section('content')
+    <div class="container">
+      @if (session('success_message'))
+          <div class="alert alert-success">
+              {{ session('success_message') }}
+          </div>
+      @endif
+      @if(count($errors) > 0)
+          <div class="alert alert-danger">
+              <ul>
+                  @foreach ($errors->all() as $error)
+                      <li>{{ $error }}</li>
+                  @endforeach
+              </ul>
+          </div>
+      @endif
+      <form method="post" id="payment-form" action="{{ route('buy.store', ['total' => $total]) }}">
+          @csrf
+          <section>
+              <h1>Il tuo totale è: {{$total}}</h1>
+              <div class="bt-drop-in-wrapper">
+                  <div id="bt-dropin"></div>
+              </div>
+          </section>
+          <input id="nonce" name="payment_method_nonce" type="hidden" />
+          <button class="button" type="submit"><span>Test Transaction</span></button>
+      </form>
+    </div>
+@endsection
 
-    <form method="post" id="payment-form" action="{{ route('buy.store', ['total' => $total]) }}">
-        @csrf
-        <section>
-            <h1>Il tuo totale è: {{$total}}</h1>
-
-            <div class="bt-drop-in-wrapper">
-                <div id="bt-dropin"></div>
-            </div>
-        </section>
-
-        <input id="nonce" name="payment_method_nonce" type="hidden" />
-        <button class="button" type="submit"><span>Test Transaction</span></button>
-    </form>
-    
+@section('script')
     <script src="https://js.braintreegateway.com/web/dropin/1.32.0/js/dropin.min.js"></script>
     <script>
         var form = document.querySelector('#payment-form');
@@ -69,5 +63,4 @@
           });
         });
     </script>
-</body>
-</html>
+@endsection
